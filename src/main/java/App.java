4b0ec0aa973c64f63966252a2c04702b454e7672
Marija Milenkovic <makicaaa.2000@gmail.com>
2025-09-29 12:01:40 +0200

    Main metodu sam dodala u klasi App.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,16 @@
 import klijent.forme.SingInForm;
 import server.forme.ServerskaForma;
 
+public class App{
 
-void main(String[] args) {
+    public static void main(String[] args) {
 
-    Thread serverThread = new Thread(() -> ServerskaForma.main(args));
-    serverThread.setName("ServerThread");
-    serverThread.start();
+        Thread serverThread = new Thread(() -> ServerskaForma.main(args));
+        serverThread.setName("ServerThread");
+        serverThread.start();
 
-    Thread clientThread = new Thread(() -> SingInForm.main(args));
-    clientThread.setName("ClientThread");
-    clientThread.start();
+        Thread clientThread = new Thread(() -> SingInForm.main(args));
+        clientThread.setName("ClientThread");
+        clientThread.start();
+    }
 }
